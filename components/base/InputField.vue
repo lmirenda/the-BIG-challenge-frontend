190d9c5d56b6c4cbc24028d1ed345993ff2849a1
lmirenda <lucamateomirenda@gmail.com>
2022-03-25 09:55:38 -0300

    Create base form components

--- components/base/InputField.vue
+++ components/base/InputField.vue
@@ -4,18 +4,30 @@
       <slot></slot>
     </label>
     <div class="mt-1">
-      <input
+      <Field
         :name="name"
         :type="type"
         :required="required"
-        :class="className"
-        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind:class="className"
+        @change="$emit('update:modelValue', $event.target.value)"
+        @blur="handleBlur"
+        :rules="validation"
       />
+      <ErrorMessage :name="name" class="text-red-500 text-xs" />
+      <span class="text-red-500 text-xs" v-if="confirmationError"
+        >Comirmation {{ type }}s don't not match</span
+      >
     </div>
   </div>
 </template>
 
 <script setup>
+import { Field, ErrorMessage } from 'vee-validate'
+const confirmationError = ref(false)
+const className = ref(
+  'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+)
+
 const props = defineProps({
   name: {
     type: String,
@@ -29,14 +41,28 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  className: {
+  referenceValue: {
     type: String,
-    default:
-      'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+    default: null,
   },
   required: {
     type: Boolean,
     default: true,
   },
+  validation: {
+    type: Function,
+    default: null,
+  },
 })
+
+function handleBlur() {
+  if (props.referenceValue === null) {
+  } else {
+    if (props.modelValue === props.referenceValue) {
+      confirmationError.value = false
+    } else {
+      confirmationError.value = true
+    }
+  }
+}
 </script>

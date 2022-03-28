8e6373eead9447cf7d2deab89dd22cebddd7821d
lmirenda <lucamateomirenda@gmail.com>
2022-03-28 16:29:04 -0300

    Create user layour navbar component scaffolding

---
+++ components/user/nav/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="space-y-1">
+    <UserNavItemMain v-for="item in mainItems" :key="item.name" :item="item" />
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon, ViewListIcon } from '@heroicons/vue/outline'
+
+const mainItems = ref([
+  { name: 'Home', href: '/about', icon: HomeIcon, current: true },
+  {
+    name: 'My petitions',
+    href: '/petitions',
+    icon: ViewListIcon,
+    current: false,
+  },
+])
+</script>

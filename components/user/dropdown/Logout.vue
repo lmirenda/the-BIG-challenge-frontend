8ba03a7829ec385b4848252b5027fdaa0d8c3dd5
lmirenda <lucamateomirenda@gmail.com>
2022-03-28 16:58:11 -0300

    Extract logout into separate menu item component

---
+++ components/user/dropdown/Logout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <MenuItem v-slot="{ active }">
+      <span
+        @click.prevent="logout"
+        :class="[
+          active ? 'bg-gray-100 text-gray-900 cursor-pointer' : 'text-gray-700',
+          'block px-4 py-2 text-sm',
+        ]"
+      >
+        Logout
+      </span>
+    </MenuItem>
+  </div>
+</template>
+
+<script setup>
+import { MenuItem } from '@headlessui/vue'
+
+const { $apiFetch } = useNuxtApp()
+
+async function logout() {
+  try {
+    await $apiFetch('/api/logout', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('validToken'),
+      },
+    })
+    sessionStorage.setItem('validToken', '')
+    window.location.pathname = '/login'
+  } catch (err) {
+    console.log(err.data)
+  }
+}
+</script>
--- components/user/dropdown/Menu.vue
+++ components/user/dropdown/Menu.vue
@@ -22,7 +22,7 @@
           />
         </div>
         <div>
-          <UserDropdownItem :item="logoutItem" />
+          <UserDropdownLogout />
         </div>
       </MenuItems>
     </transition>

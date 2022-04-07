48536aa4e3d4ea17fd45a07583c741eacf2557b2
lmirenda <lucamateomirenda@gmail.com>
2022-04-07 10:44:26 -0300

    Create card component and adjust table button

--- components/user/home/Petition/Card.vue
+++ components/user/home/Petition/Card.vue
@@ -1,90 +1,125 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <li class="relative col-span-1 flex shadow-sm rounded-md">
-    <div
-      :class="[
-        item.bgColorClass,
-        'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md',
-      ]"
-    >
-      {{ item.initials }}
-    </div>
-    <div
-      class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate"
-    >
-      <div class="flex-1 px-4 py-2 text-sm truncate">
-        <a href="#" class="text-gray-900 font-medium hover:text-gray-600">
-          {{ item.title }}
-        </a>
-        <p class="text-gray-500">{{ item.status }}</p>
+  <div class="sm:p-6">
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <div class="flex justify-end">
+          <NuxtLink to="/user-panel">
+            <XIcon
+              class="h-7 w-7 flex justify-end hover:cursor-pointer hover:bg-gray-200"
+              aria-hidden="true"
+            />
+          </NuxtLink>
+        </div>
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          {{ petition.title }}
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          {{ petition.created_at }}
+        </p>
       </div>
-      <Menu as="div" class="flex-shrink-0 pr-2">
-        <MenuButton
-          class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-        >
-          <span class="sr-only">Open options</span>
-          <DotsVerticalIcon class="w-5 h-5" aria-hidden="true" />
-        </MenuButton>
-        <transition
-          enter-active-class="transition ease-out duration-100"
-          enter-from-class="transform opacity-0 scale-95"
-          enter-to-class="transform opacity-100 scale-100"
-          leave-active-class="transition ease-in duration-75"
-          leave-from-class="transform opacity-100 scale-100"
-          leave-to-class="transform opacity-0 scale-95"
-        >
-          <MenuItems
-            class="z-10 mx-3 origin-top-right absolute right-10 top-3 w-48 mt-1 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none"
-          >
-            <div class="py-1">
-              <MenuItem v-slot="{ active }">
-                <a
-                  href="#"
-                  :class="[
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm',
-                  ]"
-                  >View</a
+      <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div class="sm:col-span-1">
+            <dt class="text-sm font-medium text-gray-500">Full name</dt>
+            <dd class="mt-1 text-sm text-gray-900">{{ user?.name }}</dd>
+          </div>
+          <div class="sm:col-span-1">
+            <dt class="text-sm font-medium text-gray-500">Email address</dt>
+            <dd class="mt-1 text-sm text-gray-900">{{ user?.email }}</dd>
+          </div>
+          <div class="sm:col-span-1">
+            <dt class="text-sm font-medium text-gray-500">Phone number</dt>
+            <dd class="mt-1 text-sm text-gray-900">
+              {{ petition.patient?.patient_phone }}
+            </dd>
+          </div>
+          <div class="sm:col-span-1">
+            <dt class="text-sm font-medium text-gray-500">Height and Weight</dt>
+            <dd class="mt-1 text-sm text-gray-900">
+              Height: {{ petition.patient?.patient_height }} | Weight:
+              {{ petition.patient?.patient_weight }}
+            </dd>
+          </div>
+          <div class="sm:col-span-2">
+            <dt class="text-sm font-medium text-gray-500">Symptoms</dt>
+            <dd class="mt-1 text-sm text-gray-900">
+              {{ petition.symptoms }}
+            </dd>
+          </div>
+          <div class="sm:col-span-2">
+            <dt class="text-sm font-medium text-gray-500">Other Info</dt>
+            <dd class="mt-1 text-sm text-gray-900">
+              {{ petition.patient?.patient_other_info }}
+            </dd>
+          </div>
+          <div class="sm:col-span-2" v-if="petition.status != 'pending'">
+            <dt class="text-sm font-medium text-gray-500">Doctor</dt>
+            <dd class="mt-1 text-sm text-gray-900">{{ petition.doctor }}</dd>
+          </div>
+          <div class="sm:col-span-2" v-if="petition.status == 'finished'">
+            <dt class="text-sm font-medium text-gray-500">Attachments</dt>
+            <dd class="mt-1 text-sm text-gray-900">
+              <ul
+                role="list"
+                class="border border-gray-200 rounded-md divide-y divide-gray-200"
+              >
+                <li
+                  class="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
                 >
-              </MenuItem>
-            </div>
-            <div class="py-1">
-              <MenuItem v-slot="{ active }">
-                <span
-                  @click="petitionStore.unpinPetition(item.id)"
-                  :class="[
-                    active
-                      ? 'bg-gray-100 text-red-400 cursor-pointer'
-                      : 'text-gray-700',
-                    'block px-4 py-2 text-sm',
-                  ]"
-                  >Removed from pinned</span
-                >
-              </MenuItem>
-            </div>
-          </MenuItems>
-        </transition>
-      </Menu>
+                  <div class="w-0 flex-1 flex items-center">
+                    <PaperClipIcon
+                      class="flex-shrink-0 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span class="ml-2 flex-1 w-0 truncate">
+                      {{ petition.file }}
+                    </span>
+                  </div>
+                  <div class="ml-4 flex-shrink-0">
+                    <a
+                      href="#"
+                      class="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Download
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
-  </li>
+  </div>
 </template>
 
 <script setup>
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { ClockIcon, MenuAlt1Icon, ViewListIcon } from '@heroicons/vue/outline'
-import { DotsVerticalIcon } from '@heroicons/vue/solid'
-import { usePatientPetitionStore } from '@/stores/patientPetitions'
-const petitionStore = usePatientPetitionStore()
-const props = defineProps({
-  item: {
-    type: Object,
-    default: {
-      id: 3,
-      title: 'Dummy Petition',
-      initials: 'DP',
-      status: 'Unknown',
-      pinned: true,
-      bgColorClass: 'bg-pink-600',
+import { PaperClipIcon, XIcon } from '@heroicons/vue/solid'
+
+const { $apiFetch } = useNuxtApp()
+const petition = ref([])
+const route = useRoute()
+const user = ref()
+
+async function getPetition() {
+  let data = await $apiFetch('/api/my/petitions', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'X-XSRF-TOKEN': sessionStorage.getItem('validToken'),
+      Authorization: 'Bearer ' + sessionStorage.getItem('validToken'),
     },
-  },
-})
+  })
+  let array = data[0]
+  let id = route.params.id
+  petition.value = findPetitionById(array, id)
+  user.value = JSON.parse(sessionStorage.getItem('user'))
+}
+
+function findPetitionById(array, id) {
+  return array.find((petition) => petition.id == id)
+}
+
+onBeforeMount(getPetition())
 </script>

b0b3a008f62fff3b686005fc88910bb044e25d26
lmirenda <lucamateomirenda@gmail.com>
2022-05-03 12:09:33 -0300

    Create upload and form components

--- components/doctor/AcceptedCard.vue
+++ components/doctor/AcceptedCard.vue
@@ -1,10 +1,9 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="sm:p-6">
     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
       <div class="px-4 py-5 sm:px-6">
         <div class="flex justify-end">
-          <NuxtLink to="/doctor-panel">
+          <NuxtLink to="/petitions/doctor/accepted/">
             <XIcon
               class="h-7 w-7 flex justify-end hover:cursor-pointer hover:bg-gray-200"
               aria-hidden="true"
@@ -33,13 +32,13 @@
           <div class="sm:col-span-1">
             <dt class="text-sm font-medium text-gray-500">Phone number</dt>
             <dd class="mt-1 text-sm text-gray-900">
-              {{ petition.patient?.patient_phone }}
+              {{ petition?.patient?.patient_phone }}
             </dd>
           </div>
           <div class="sm:col-span-1">
             <dt class="text-sm font-medium text-gray-500">Height and Weight</dt>
             <dd class="mt-1 text-sm text-gray-900">
-              Height: {{ petition.patient?.patient_height }} | Weight:
+              Height: {{ petition?.patient?.patient_height }} | Weight:
               {{ petition?.patient?.patient_weight }}
             </dd>
           </div>
@@ -65,22 +64,36 @@
             <div class="flex-shrink-0">
               <button
                 type="button"
+                :disabled="showAlert"
                 @click.prevent="acceptPetition"
-                class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class="disabled:bg-indigo-200 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Accept
+                {{ !showAlert ? 'Accept' : 'Accepted' }}
               </button>
             </div>
           </div>
           <div class="sm:col-span-2" v-if="petition?.status == 'in progress'">
-            <BaseDragUpload />
+            <BaseDragUpload @drop.prevent="drop" @change="selectedFile" />
+            <dd class="mt-1 text-sm text-gray-900">
+              {{ dropzoneFile?.name }}
+            </dd>
           </div>
         </dl>
       </div>
     </div>
-    <AlertSuccess v-if="showAlert == true" @close="close"
-      >Petition accepted</AlertSuccess
-    >
+    <div class="mt-6">
+      <AlertSuccess v-if="showAlert == true" @close="close">
+        <div class="inline-block">
+          Petition accepted successfully.
+          <span
+            class="text-blue-500 hover:text-blue-600 hover:cursor-pointer"
+            @click="reload"
+          >
+            <NuxtLink> Click here to upload a prescription. </NuxtLink>
+          </span>
+        </div>
+      </AlertSuccess>
+    </div>
   </div>
 </template>
 
@@ -92,6 +105,23 @@ const petition = ref()
 const route = useRoute()
 const user = ref()
 const showAlert = ref(false)
+const dropzoneFile = ref('')
+
+const selectedFile = () => {
+  dropzoneFile.value = document.querySelector('input[type=file]').files[0]
+}
+
+const drop = (e) => {
+  dropzoneFile.value = e.dataTransfer.files[0]
+}
+
+const close = () => {
+  showAlert.value = false
+}
+
+const reload = () => {
+  window.location.reload()
+}
 
 async function getPetition() {
   petition.value = await $apiFetch('/api/petitions/' + route.params.id, {
@@ -102,22 +132,21 @@ async function getPetition() {
       Authorization: 'Bearer ' + sessionStorage.getItem('validToken'),
     },
   })
-  console.log(petition.value)
 }
 
 async function acceptPetition() {
-  await $apiFetch('/api/petitions/accept/' + petition.value.id, {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      'X-XSRF-TOKEN': sessionStorage.getItem('validToken'),
-      Authorization: 'Bearer ' + sessionStorage.getItem('validToken'),
+  await $apiFetch(
+    '/api/petitions/accept/' + petition.value.id,
+    {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'X-XSRF-TOKEN': sessionStorage.getItem('validToken'),
+        Authorization: 'Bearer ' + sessionStorage.getItem('validToken'),
+      },
     },
-  })
-  console.log(response)
-  if (response.status === 200) {
-    console.log('success')
-  }
+    (showAlert.value = true)
+  )
 }
 
 onBeforeMount(getPetition())
